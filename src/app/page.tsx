0928c2d1e9c29dc90301365eb4f06b1e332ef48b
soreikeonti-web <soreikeonti@gmail.com>
2025-10-12 14:11:40 +0900

    docs: トップページのテキストを更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,18 +12,8 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      
+      <h1 className="text-4xl font-bold">占いサイトへようこそ！</h1>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
