cf2f0794b63dfc871f9082043e72f9eb3cfe0fb2
soreikeonti-web <soreikeonti@gmail.com>
2025-10-17 21:08:19 +0900

    tailwind.config修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./src/app/**/*.{js,ts,jsx,tsx}",
-      "./src/components/**/*.{js,ts,jsx,tsx}",
-      "./src/**/*.{js,ts,jsx,tsx}",
+      "src/app/**/*.{js,ts,jsx,tsx}",
+      "src/components/**/*.{js,ts,jsx,tsx}",
+      "src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
     },
     plugins: [],
-  }
+  };
   
