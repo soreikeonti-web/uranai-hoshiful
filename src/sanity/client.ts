e1ef87378e8155bdb88c7f73c63b9145ed437143
soreikeonti-web <soreikeonti@gmail.com>
2025-10-18 23:02:39 +0900

    client.ts を修正

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,10 +1,9 @@
-import {createClient} from 'next-sanity'
-import {apiVersion, dataset, projectId} from './env'
+import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: '2023-01-01', // 任意の日付でもOK
   useCdn: false,
   perspective: 'published',
 })
